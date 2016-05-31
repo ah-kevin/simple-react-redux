9adbeeeb2f218254dffcf874a2d5a0f2f9ebd9d8
dg_lennon <kbjwdh@gmail.com>
2016-05-31 13:25:12 +0800

    immutable添加版本

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import configureStore from './stores/configureStore';
 import {browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 import createRoutes from './routes/routes';
+
 const store = configureStore();
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState (state) {
+    return state.get('routing').toJS();
+  }
+});
 ReactDOM.render(
   <Provider store={store} children="">
-    {createRoutes(history)}
+            {createRoutes(history)}
   </Provider>
   , document.getElementById('app'));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,23 @@
 /**
  * Created by dg_lennon on 16/5/31.
  */
-import {combineReducers} from 'redux';
-import {routerReducer} from 'react-router-redux';
+import {combineReducers} from 'redux-immutable';
+import {LOCATION_CHANGE} from 'react-router-redux';
+import Immutable from 'immutable';
+
+const initialState = Immutable.fromJS({
+  locationBeforeTransitions: null
+});
+function routerReducer(state = initialState, action){
+  if (action.type === LOCATION_CHANGE) {
+    return state.merge({
+      locationBeforeTransitions: action.payload
+    });
+  }
+
+  return state;
+}
+
 //使用redux的combineReducers方法将所有reducer打包起来
 const rootReducer = combineReducers({
   routing:routerReducer
